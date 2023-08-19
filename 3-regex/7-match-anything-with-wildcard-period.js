8172e0d1ee10d1bdc1e15ff78c4437f1f1dd19c3
Moktadir Rahman <autocircled@gmail.com>
2023-08-19 11:21:00 +0600

    es6 1-7 done

---
+++ 3-regex/7-match-anything-with-wildcard-period.js
@@ -0,0 +1,6 @@
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+console.log(result);
+let res = exampleStr.match(unRegex);
+console.log(res);
